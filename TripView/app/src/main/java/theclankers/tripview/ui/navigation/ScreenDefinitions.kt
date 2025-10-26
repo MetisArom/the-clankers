adce26c1a11c417dccd0d524b255aabce088b155
Jade Redmond <redjade@umich.edu>
2025-10-26 15:01:16 -0400

    Basic Skeleton for trips screen
    Header text for trip screen with comments about which components for us to add later on.

--- TripView/app/src/main/java/theclankers/tripview/ui/navigation/ScreenDefinitions.kt
+++ TripView/app/src/main/java/theclankers/tripview/ui/navigation/ScreenDefinitions.kt
@@ -1,16 +1,33 @@
 package theclankers.tripview.ui.navigation
 
-import android.R.attr.text
+import android.R.id.bold
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.FontWeight.Companion.Bold
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import theclankers.tripview.ui.theme.Purple80
+import theclankers.tripview.ui.theme.PurpleGrey80
+
 
 @Composable
-fun HomeScreen(navController: NavHostController) { Button(onClick = {
-    navigateTo(navController, "navigation")
-}) { Text("Go to Navigation Screen")  } }
+fun HomeScreen(navController: NavHostController) {
+    Column() {
+        Button(onClick = {
+            navigateTo(navController, "navigation")
+        }) { Text("Go to Navigation Screen") }
+
+        Text(text = "Your Trips", fontSize = 40.sp, color = Purple80)// need this darker
+        //Add component/button for create a trip
+        // add list component for user's trip
+        Text(text = "Friends' Trips", fontSize = 40.sp, color = Purple80)// need this darker
+        //add list component for friend trips
+        Text(text = "Completed Trips", fontSize = 40.sp, color = Purple80)// need this darker
+        // add list component for friend trips
+    }
+}
 @Composable
 fun CameraScreen(navController: NavHostController) { Button(onClick = {
     navigateTo(navController, "camera2")
