718cccffeb7f7bf18001b5036cc237ffb6d25428
jorelm68 <methan@umich.edu>
2025-10-28 18:54:01 -0400

    Added skeleton files for all the high-level front-end stuff

---
+++ TripView/app/src/main/java/theclankers/tripview/ui/viewmodel/UserViewModel.kt
@@ -0,0 +1,4 @@
+package theclankers.tripview.ui.viewmodel
+
+// Use this ViewModel for grabbing state relevant to a specific user.
+// For example, pass as input "user_id" and it will return variables like "first_name", "last_name", and "username"
