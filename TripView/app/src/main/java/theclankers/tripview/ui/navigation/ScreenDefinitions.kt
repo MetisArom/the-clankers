116457c74b590a12dc1b41d14e4929bb13977574
Jade Redmond <redjade@umich.edu>
2025-10-29 15:21:44 -0400

    Basic Skeleton for trips screen
    Header text for trip screen with comments about which components for us to add later on.
    
    # Conflicts:
    #	TripView/app/src/main/java/theclankers/tripview/ui/navigation/ScreenDefinitions.kt

--- TripView/app/src/main/java/theclankers/tripview/ui/navigation/ScreenDefinitions.kt
+++ TripView/app/src/main/java/theclankers/tripview/ui/navigation/ScreenDefinitions.kt
@@ -1,5 +1,6 @@
 package theclankers.tripview.ui.navigation
 
+import android.R.id.bold
 
 
 import androidx.compose.foundation.background
@@ -11,14 +12,35 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.FontWeight.Companion.Bold
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import theclankers.tripview.ui.theme.Purple80
+import theclankers.tripview.ui.theme.PurpleGrey80
+
 import theclankers.tripview.ui.components.ProfilePageComponent
 import theclankers.tripview.ui.components.TripItem
 
+@Composable
+fun HomeScreen(navController: NavHostController) {
+    Column() {
+        Button(onClick = {
+            navigateTo(navController, "navigation")
+        }) { Text("Go to Navigation Screen") }
+
+        Text(text = "Your Trips", fontSize = 40.sp, color = Purple80)// need this darker
+        //Add component/button for create a trip
+        // add list component for user's trip
+        Text(text = "Friends' Trips", fontSize = 40.sp, color = Purple80)// need this darker
+        //add list component for friend trips
+        Text(text = "Completed Trips", fontSize = 40.sp, color = Purple80)// need this darker
+        // add list component for friend trips
+    }
+}
 @Composable
 fun CameraScreen(navController: NavHostController) { Button(onClick = {
     navigateTo(navController, "camera2")
@@ -62,3 +84,13 @@ fun ProfileScreen(navController: NavController) {
 
 @Composable
 fun Camera2Screen() { Text("Camera 2 Screen") }
+
+@Composable
+fun StopScreen(navController: NavHostController, itemId: Int?) {
+    Column {
+        Text("This is the stop page for stop $itemId")
+        Button(onClick = {
+            goBack(navController)
+        }) { Text("Go back") }
+    }
+}
