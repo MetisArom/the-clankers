c5c93813400b3399db7009431c5b3127607fc21b
jorelm68 <methan@umich.edu>
2025-10-30 17:43:48 -0400

    Implemented sample getUser function

--- TripView/app/src/main/java/theclankers/tripview/data/api/ApiClient.kt
+++ TripView/app/src/main/java/theclankers/tripview/data/api/ApiClient.kt
@@ -2,10 +2,24 @@ package theclankers.tripview.data.api
 
 import kotlinx.serialization.json.Json
 import theclankers.tripview.utils.Constants.BASE_URL
+import okhttp3.Request
+import theclankers.tripview.utils.HttpHelper
+import java.io.IOException
 
 object ApiClient {
     private val json = Json { ignoreUnknownKeys = true }
 
-    suspend fun getUser(userId: Int): String
+    suspend fun getUser(userId: Int): String {
+        val url = "$BASE_URL/users/$userId"
+        val request = Request.Builder()
+            .url(url)
+            .get()
+            .build()
 
+        val response = HttpHelper.get(request)
+        if (!response.isSuccessful) {
+            throw IOException("Request failed: ${response.code}")
+        }
+        return response.body.string()
+    }
 }
