b0f45cde536de969c7e8c1ceb3128aabb815c3f5
Flizzyclone <stormry20@gmail.com>
2025-11-06 12:45:55 -0500

    Change back to local BASE_URL and uncomment 500 error :(

--- TripView/app/src/main/java/theclankers/tripview/core/Constants.kt
+++ TripView/app/src/main/java/theclankers/tripview/core/Constants.kt
@@ -5,10 +5,10 @@ object Constants {
     //const val BASE_URL = "https://efc1c087fe90.ngrok-free.app"
 
     //localhost BASE_URL (for testing on backend running on local machine)
-    //const val BASE_URL = "http://10.0.2.2:5000"
+    const val BASE_URL = "http://10.0.2.2:5000"
 
     //hosted backend for demo
-    const val BASE_URL = "https://localitydb.ryankirby.dev"
+    //const val BASE_URL = "https://localitydb.ryankirby.dev"
 
     const val USERNAME = "alice"
     const val PASSWORD = "easy"
