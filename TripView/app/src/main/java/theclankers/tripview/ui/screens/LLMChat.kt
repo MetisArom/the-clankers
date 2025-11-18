e9536ea42997042854b94cf5d744874fa2b0a766
Jade Redmond <redjade@umich.edu>
2025-11-17 20:58:20 -0500

    llm chat setup?

--- TripView/app/src/main/java/theclankers/tripview/ui/screens/LLMChat.kt
+++ TripView/app/src/main/java/theclankers/tripview/ui/screens/LLMChat.kt
@@ -1,3 +1,11 @@
 package theclankers.tripview.ui.screens
 
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
 //look at Figma, code same functionality
+
+@Composable
+fun ChatScreen() {
+    Text("LLMChat")
+}
